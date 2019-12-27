f98d907ae3d51f522db7653d607391baa2b37694
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2019-12-27 14:58:06 +0530

    Implemented basic gameplay

--- main.js
+++ main.js
@@ -3,35 +3,24 @@
 function createPlayer(nameArg){
   let name = nameArg;
   let myMoves = [];
-  let makeMove = function(cell){
-    myMoves.push(cell);
-  }
-  return { name, makeMove };
+  return { name };
 }
 
 
 //GameBoard module
 const GameBoard = ( () => {
   let gameboard = {
-    '#a1': 'A',
-    '#a2': 'B',
-    '#a3': 'C',
-    '#b1': 'D',
-    '#b2': 'E',
-    '#b3': 'F',
-    '#c1': 'G',
-    '#c2': 'H',
-    '#c3': 'I'
+    '#a1': '',
+    '#a2': '',
+    '#a3': '',
+    '#b1': '',
+    '#b2': '',
+    '#b3': '',
+    '#c1': '',
+    '#c2': '',
+    '#c3': ''
   };
 
-  let applyEventListeners = function(){
-    for(let key in gameboard){
-      let cellQuery = document.querySelector(key);
-      if( gameboard.hasOwnProperty(key)){
-        cellQuery.addEventListener('click', (e)=> console.log('hey ', e.target.id));
-      }
-    }
-  };
   //populate game board with values in gameboard array
   let populateGameBoard = function(){
     for(let key in gameboard){
@@ -45,6 +34,7 @@ const GameBoard = ( () => {
   let assignValue = function(cell, value){
     if(gameboard[cell] === ''){
       gameboard[cell] = value;
+      populateGameBoard();
     }else{
       throw new Error("Cannot assign a new value to a populated cell.")
     }
@@ -52,16 +42,16 @@ const GameBoard = ( () => {
   let getGameBoard = function(){
     return gameboard;
   }
-  return {getGameBoard, assignValue, populateGameBoard, applyEventListeners};
+  return {getGameBoard, assignValue, populateGameBoard};
 })();
 
 
 // Gameplay Module
 const Gameplay = ( () => {
-  let whoseTurn
   let gameStarted = false;
   let player1 = createPlayer("Player 1");
   let player2 = createPlayer("Player 2");
+  let turn = true;
   let applyEventListeners = function(){
     let startBtn = document.querySelector("#startBtn");
     startBtn.addEventListener('click', ()=> initializeGame() );
@@ -85,15 +75,33 @@ const Gameplay = ( () => {
       throw new Error("Game already started.")
     }
   }
-  return{ applyEventListeners, player1, player2 }
+  let makeAMove = function(){
+    let gb = GameBoard.getGameBoard();
+    console.log('gb: ', gb)
+    for(let key in gb){
+      let cellQuery = document.querySelector(key);
+      if( gb.hasOwnProperty(key)){
+        console.log('inside MAM')
+        cellQuery.addEventListener('click', (e)=> {
+          if(turn){
+            GameBoard.assignValue(key, 'O')
+            turn = !turn;
+          }else{
+            GameBoard.assignValue(key, 'X')
+            turn = !turn;
+          }
+        });
+      }
+    }
+  };
+
+  return{ applyEventListeners, makeAMove, player1, player2 }
 })();
 
 function render(){
-  //let myGame = GameBoard();
   GameBoard.populateGameBoard();
-  GameBoard.applyEventListeners();
   Gameplay.applyEventListeners();
-
+  Gameplay.makeAMove();
 }
 
 render();
